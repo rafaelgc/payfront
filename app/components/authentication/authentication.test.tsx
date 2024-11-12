335265ef7b80b7ed93c7a5d28f19131280ddd536
Rafa <rafa@rafadebian.rgonzalez.xyz>
2024-11-12 23:08:33 +0100

    Check dispatch.

--- app/components/authentication/authentication.test.tsx
+++ app/components/authentication/authentication.test.tsx
@@ -1,21 +1,50 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import { Authentication } from './authentication';
+import { ACTIONS, StoreContext, initialState } from '@/store';
 
 const localStorageMock = {
   getItem: jest.fn(),
 }
 
 describe('Authentication', () => {
-  it('renders without crashing', () => {
+  beforeEach(() => {
+    localStorageMock.getItem.mockReset();
+  });
+
+  it('set token is dispatched', () => {
+    const dispatch = jest.fn();
 
     Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
     localStorageMock.getItem.mockReturnValueOnce('fake_token');
 
-    render(<Authentication useHeap={false} />);
+    render(
+      <StoreContext.Provider value={{ state: initialState, dispatch }}>
+        <Authentication useHeap={false} />
+      </StoreContext.Provider>
+    );
     expect(localStorageMock.getItem).toHaveBeenCalledTimes(1);
     expect(localStorageMock.getItem).toHaveBeenCalledWith('token');
+    expect(dispatch).toHaveBeenCalledWith({ type: ACTIONS.SET_TOKEN, payload: 'fake_token' });
+
+  });
+
+  it('token not loading', () => {
+    const dispatch = jest.fn();
+
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+    localStorageMock.getItem.mockReturnValueOnce(undefined);
+
+    render(
+      <StoreContext.Provider value={{ state: initialState, dispatch }}>
+        <Authentication useHeap={false} />
+      </StoreContext.Provider>
+    );
+    expect(localStorageMock.getItem).toHaveBeenCalledTimes(1);
+    expect(localStorageMock.getItem).toHaveBeenCalledWith('token');
+    expect(dispatch).toHaveBeenCalledWith({ type: ACTIONS.SET_NOT_LOADING_TOKEN });
 
   });
 });
